5361fafafc62d1a0a3339e2ecf4cc40ecd232a76
oscaromar22 <63162076+oscaromar22@users.noreply.github.com>
2020-04-05 23:24:42 -0500

    Create Herencia.java

---
+++ herencia/src/herencia/Herencia.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author: Olivera Gutierrez Oscar Omar
+ */
+public class Herencia {
+
+    public static void main(String[] args) {
+        
+        Dog doguno = new Dog();
+        doguno.LlenarDatosPerro();
+        doguno.VerSalida();
+        
+        Dog dogdos = new Dog();
+        dogdos.LlenarDatosPerro();
+        dogdos.VerSalida();
+        
+        Dog dogtres = new Dog();
+        dogtres.LlenarDatosPerro();
+        dogtres.VerSalida();
+        
+
+
+    }
+}
+
+
