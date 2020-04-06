8c108f918009ee93886b8a5f071e97c32d4394bc
oscaromar22 <63162076+oscaromar22@users.noreply.github.com>
2020-04-05 23:24:59 -0500

    Create Dog2.java

---
+++ herencia/src/herencia/Dog2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author: Olivera Gutierrez Oscar Omar
+ */
+public class Dog2 extends Dog {
+
+	double portion;
+
+}
